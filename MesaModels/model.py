1abc2737dd95e9c11a0a58d41fb5997339924520
Diego-CP <pinedo.dc@gmail.com>
2022-12-01 18:10:49 -0600

    Fast and furious spawning

--- MesaModels/model.py
+++ MesaModels/model.py
@@ -54,7 +54,7 @@ class City(Model):
 
 
         # Set spawn points at the corners of the grid
-        self.spawns = [(0, self.height-2), (self.width - 1, 0)]
+        self.spawns = [(0, self.height-1), (self.width - 1, 0)]
         self.a_star = AStar(maze)   
         self.running = True 
         self.num_agents = N
@@ -66,10 +66,10 @@ class City(Model):
         cars_in_model = sum([1 if isinstance(test_agent, Car) else 0 for test_agent in self.schedule.agents])
         cars = 0
         # Spawn a car, unless there are more than num_agents
-        if cars_in_model < self.num_agents and self.schedule.steps % 4 == 0:
+        if cars_in_model < self.num_agents and self.schedule.steps % 2 == 0:
             spawn_point = self.spawns[cars_in_model % 2]
-            # Check if there is a car at the spawn point
-            cars_in_spawn = sum([1 if isinstance(test_agent, Car) else 0 for test_agent in self.grid.get_cell_list_contents(spawn_point)])
+            # Check if there is a car at or around the spawn point
+            cars_in_spawn = sum([1 if isinstance(test_agent, Car) else 0 for test_agent in self.grid.iter_neighbors(spawn_point, True, True, 1)])
             if cars_in_spawn == 0:
                 fate = self.random.choice(self.destinations)
                 route = self.a_star.search(1, spawn_point, fate)
